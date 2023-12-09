9b63bc7827a72a6f648311929d3cde03e802042d
sybil-lang <charuprabha228@gmail.com>
2023-12-09 11:08:09 +0530

    doing chnages

--- Frontend/src/components/Public.js
+++ Frontend/src/components/Public.js
@@ -9,7 +9,7 @@ const Public = () => {
                 <div className="text-center">
                     <h1 className="max-w-md mx-auto text-3xl font-extrabold tracking-normal text-gray-900 sm:text-3xl md:text-5xl lg:text-6xl md:leading-none sm:max-w-xl md:max-w-2xl lg:max-w-4xl">
                         <span className="block">
-                       Welccome to my App
+                       Welcome to my App
                         </span>
                     </h1>
                    
